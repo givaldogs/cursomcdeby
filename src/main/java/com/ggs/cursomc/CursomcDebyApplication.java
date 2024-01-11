ae3bf4c1b67038bda47044fcf5b305ee5808b400
givaldogs <givaldowanda@gmail.com>
2024-01-11 19:35:40 -0300

    projeto criado

---
+++ src/main/java/com/ggs/cursomc/CursomcDebyApplication.java
@@ -0,0 +1,13 @@
+package com.ggs.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcDebyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcDebyApplication.class, args);
+	}
+
+}
