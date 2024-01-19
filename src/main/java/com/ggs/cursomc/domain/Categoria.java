3b6a245e0fdda61cf064a94bcfdf67bfd837c80c
givaldogs <givaldowanda@gmail.com>
2024-01-19 20:01:43 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/ggs/cursomc/domain/Categoria.java
+++ src/main/java/com/ggs/cursomc/domain/Categoria.java
@@ -3,6 +3,12 @@ package com.ggs.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	
 	/**
@@ -10,6 +16,8 @@ public class Categoria implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
