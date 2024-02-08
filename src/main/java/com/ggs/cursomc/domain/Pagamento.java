b83c00e16913bb876dd549a2f6a28bf128fde2a3
givaldogs <givaldowanda@gmail.com>
2024-02-08 09:40:10 -0300

    Endpoint/pedidos/{id} disponibilizado

--- src/main/java/com/ggs/cursomc/domain/Pagamento.java
+++ src/main/java/com/ggs/cursomc/domain/Pagamento.java
@@ -3,6 +3,7 @@ package com.ggs.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.ggs.cursomc.domain.enums.EstadoPagamento;
 
 import jakarta.persistence.Entity;
@@ -23,6 +24,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
--- src/main/java/com/ggs/cursomc/domain/Produto.java
+++ src/main/java/com/ggs/cursomc/domain/Produto.java
@@ -10,6 +10,7 @@ import java.util.Set;
 import org.hibernate.annotations.ManyToAny;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -38,6 +39,7 @@ public class Produto implements Serializable {
 
 	private List<Categoria> categorias = new ArrayList<>();
 
+	@JsonIgnore
 	@OneToMany(mappedBy = "id.produto")
 	private Set<ItemPedido> items = new HashSet<>();
 
@@ -52,6 +54,7 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 
+	@JsonIgnore
 	public List<Pedido> getPedidos(){		
 		List<Pedido> lista = new ArrayList<>();
 		for(ItemPedido x: items)
