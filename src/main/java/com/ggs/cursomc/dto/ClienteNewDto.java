dccdf3fad169156862575a7a47882059f457fb9e
givaldogs <givaldowanda@gmail.com>
2024-03-07 10:34:24 -0300

    Inserindo um novo Cliente com POST, correcao ClienteNewDto

--- src/main/java/com/ggs/cursomc/dto/ClienteNewDto.java
+++ src/main/java/com/ggs/cursomc/dto/ClienteNewDto.java
@@ -2,28 +2,27 @@ package com.ggs.cursomc.dto;
 
 import java.io.Serializable;
 
-public class ClienteNewDto implements Serializable{
+public class ClienteNewDto implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private String nome;
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-	
-	private Integer id;
+
 	private String logradouro;
 	private String numero;
 	private String complemento;
 	private String bairro;
 	private String cep;
-	
+
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
-	
+
 	private Integer cidadeId;
-	
-	public ClienteNewDto() {		
+
+	public ClienteNewDto() {
 	}
 
 	public String getNome() {
@@ -56,14 +55,6 @@ public class ClienteNewDto implements Serializable{
 
 	public void setTipo(Integer tipo) {
 		this.tipo = tipo;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
 	}
 
 	public String getLogradouro() {
