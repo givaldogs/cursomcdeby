152359d5ad4430ad4b1e206f0c8a7353a1053a35
givaldogs <givaldowanda@gmail.com>
2024-03-23 18:58:43 -0300

    Subtotal e total de Pedido

--- src/main/java/com/ggs/cursomc/domain/ItemPedido.java
+++ src/main/java/com/ggs/cursomc/domain/ItemPedido.java
@@ -33,12 +33,15 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
 
-	
 	public Produto getProduto() {
 		return id.getProduto();
 	}
@@ -92,5 +95,4 @@ public class ItemPedido implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-	
 }
